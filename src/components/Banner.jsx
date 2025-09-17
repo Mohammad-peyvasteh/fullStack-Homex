98db9666957ec7ed4611a3f8721a987e4c70d1a9
Mohammad-peyvasteh <126290902+Mohammad-peyvasteh@users.noreply.github.com>
2025-09-17 17:29:49 +0330

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,31 +3,38 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <section className=" container pt-40 mx-auto">
-      <div className=" flex items-center justify-around">
-        <div className=" hidden xl:block">
-        <Image
-            width={300}
-            height={200}
-            alt=" "
-            src="/images/baner/baner2.png"
-            className=""
+    <section className="px-6 sm:px-8   max-w-7xl pt-40 py-5 mx-auto xl:px-6">
+    <div className="flex items-center gap-10 justify-center">
+  <div className="hidden xl:block ">
+    
+    <Image
+      width={320}
+      height={480}
+      alt=" "
+      src="/images/baner/baner2.png"
+    />
+  </div>
+  <div className="relative">
+    <div className=" absolute z-20 inset-0 w-full bg-blue-400/15 object-cover rounded-xl">
+    <div className=" absolute inset-x-0 bottom-0  backdrop-blur-sm rounded-sm pr-3 py-2">
+      <h2 className=" text-white font-bold md:text-3xl sm:text-2xl text-xl ">هومکس ,</h2>
+      <p className=" text-blue-200">خانه ای که با شما فکر میکند</p>
 
-          />
-            
-        </div>
-        <div>
-          <Image
-            width={870}
-            height={200}
-            alt=" "
-            src="/images/baner/baner1.png"
-            className=""
-          />
-        </div>
-      </div>
+    </div>
+
+    </div>
+    <Image
+      width={927}
+      height={480}
+      alt=" "
+      src="/images/baner/baner1.png"
+    />
+  </div>
+</div>
+
     </section>
   );
 };
 
 export default Banner;
+

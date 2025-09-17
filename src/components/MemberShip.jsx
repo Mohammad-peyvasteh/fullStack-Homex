0e26b50273298169b97bd664567aaf5722d8e238
Mohammad-peyvasteh <126290902+Mohammad-peyvasteh@users.noreply.github.com>
2025-09-17 17:32:01 +0330

    Update MemberShip.jsx

--- src/components/MemberShip.jsx
+++ src/components/MemberShip.jsx
@@ -3,7 +3,7 @@ import { IoLogoInstagram } from "react-icons/io";
 import { RiTelegramLine } from "react-icons/ri";
 const MemberShip = () => {
   return (
-   <section className="container mx-auto py-20 px-4">
+   <section className="max-w-7xl mx-auto py-20 px-6 sm:px-8 md:px-10 xl:px-6 ">
   <div className="flex flex-col items-center gap-7 text-center">
     <h1 className="font-bold text-primary-800 xl:text-3xl lg:text-2xl  text-xl">
       عضویت در باشگاه مشتریان
@@ -39,23 +39,3 @@ const MemberShip = () => {
 
 export default MemberShip;
 
-
-
-// <section className=" container mx-auto">
-//       <div className="flex flex-col items-center gap-7">
-//         <h1 className=" font-bold text-primary-800 text-3xl">عضویت در باشگاه مشتریان</h1>
-//         <p className=" text-tertiary-900">
-//           با عضویت در باشگاه مشتریان از پروژه و محصولات جدید با خبر شوید و کد
-//           تخفیف ویژه هر ماه را دریافت کنید
-//         </p>
-//         <div className=" flex items-center gap-3">
-//             <button className=" bg-primary-700 text-primary-50 py-2 px-4 rounded-lg">عضویت</button>
-//             <input type="text"  placeholder="آدرس ایمیل خود را وارد کنید ..." className=" rounded-lg py-2 px-6 outline-none border border-primary-700" />
-//         </div>
-//         <div className="flex flex-col items-center gap-2
-//         ">
-//           <h2 className="text-primary-800 font-bold">کانال ها و صفحه های ما </h2>
-//           <span className="flex items-center text-primary-800 text-2xl"><IoLogoInstagram/><RiTelegramLine/></span>
-//         </div>
-//       </div>
-//     </section>
